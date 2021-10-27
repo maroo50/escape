664590cc8f69b12fcccdf9a1d1c269dcb7b84fcd
Kim Sanghyeon <66934719+kuk6467@users.noreply.github.com>
2021-10-27 10:47:23 +0900

    211027_number250

--- Sanghyun/2020_정보처리기사_1장_코드모음/Section 008/tempCodeRunnerFile.c
+++ Sanghyun/2020_정보처리기사_1장_코드모음/Section 008/tempCodeRunnerFile.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-main()
-{
-	int a = 50;
-	int* b;
-	b = &a;
-	*b = *b + 20;
-	printf("%d, %d", a, *b);
+main() {
+	int a[5], b = 1, sum = 0;
+	for (int i = 0; i < 5; i++) {
+		a[i] = b;
+		b *= 2;
+	}
+	for (int i = 0; i < 5; i += 2)
+		sum += *(a + i);
+	printf("%d", sum);
 }
---
+++ Sanghyun/2020_정보처리기사_1장_코드모음/Section 008/기출_따라잡기_문제1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+	char* str;
+	str = "KOREA";
+	printf("%8.3s\n", str);
+}
+
+// #include <stdio.h>
+// main()
+// { 
+// 	char* str;
+// 	str = "KOREA";
+// 	printf("%10.4s\n", str);
+// }
+
+//      kor
+// --------
